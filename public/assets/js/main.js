922adb364d484cac96c24d999611924091994ae5
BethSun <beth.sun.toronto@gmail.com>
2024-04-04 22:11:47 -0400

    tickets & tasks

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,16 +7,22 @@ window.addEventListener("load", () => {
 
 function renderConent({ overview, trends, unresolvedTickets, tasks }) {
   const overviewContainer = document.querySelector("#overview");
-  overview
-    .map(createOverviewCard)
-    .forEach((card) => overviewContainer.appendChild(card));
+  overview.map(renderOverview).forEach((e) => overviewContainer.appendChild(e));
+
   const trendsContainer = document.querySelector("#trends");
-  trends
-    .map(createTrendsTile)
-    .forEach((tile) => trendsContainer.appendChild(tile));
+  trends.map(renderTrends).forEach((e) => trendsContainer.appendChild(e));
+
+  const unresolvedTicketsContainer =
+    document.querySelector("#unresolvedTickets");
+  unresolvedTickets
+    .map(renderUnresolvedTickets)
+    .forEach((e) => unresolvedTicketsContainer.appendChild(e));
+
+  const tasksContainer = document.querySelector("#tasks");
+  tasks.map(renderTasks).forEach((e) => tasksContainer.appendChild(e));
 }
 
-function createOverviewCard({ name, count }) {
+function renderOverview({ name, count }) {
   /* <div class="col-12 col-sm-6 col-md-3">
     <div class="card">
         <div class="card-body d-flex flex-column align-items-center justify-content-center">
@@ -50,7 +56,7 @@ function createOverviewCard({ name, count }) {
   return d1;
 }
 
-function createTrendsTile({ name, value }) {
+function renderTrends({ name, value }) {
   /* <li class="list-group-item d-flex flex-column align-items-center justify-content-center flex-grow-1">
     <h3 class="fs-6 text-secondary">Resolved</h3><span class="fw-bold">449</span>
 </li> */
@@ -71,5 +77,72 @@ function createTrendsTile({ name, value }) {
   span.textContent = value;
   li.appendChild(h);
   li.appendChild(span);
+  return li;
+}
+
+function renderUnresolvedTickets({ name, value }) {
+  /* <li class="list-group-item d-flex justify-content-between">
+    <span class="fw-bold">Waiting on Feature Request</span>
+    <span class="text-secondary">4238</span>
+</li>*/
+  const li = document.createElement("li");
+  li.classList.add("list-group-item", "d-flex", "justify-content-between");
+  const s1 = document.createElement("span");
+  s1.classList.add("fw-bold");
+  s1.textContent = name;
+  const s2 = document.createElement("span");
+  s2.classList.add("text-secondary");
+  s2.textContent = value;
+  li.appendChild(s1);
+  li.appendChild(s2);
+  return li;
+}
+
+let taskCounter = 0; // to get unique id for task checkbox input and label
+
+function renderTasks({ name, category, complete }) {
+  /* <li class="list-group-item d-flex justify-content-between">
+<div class="form-check form-check-inline">
+    <input class="form-check-input rounded-circle" type="checkbox" name="tasks"
+    value="Finish Ticket Update" id="task1">
+    <label class="form-check-label fw-bold" for="task1">Finish ticket update</label>
+</div>
+<span class="badge text-bg-warning d-flex align-items-center">URGENT</span>
+</li>*/
+  const taskId = `task${++taskCounter}`;
+  const li = document.createElement("li");
+  li.classList.add("list-group-item", "d-flex", "justify-content-between");
+
+  const d = document.createElement("div");
+  d.classList.add("form-check", "form-check-inline");
+  const input = document.createElement("input");
+  input.classList.add("form-check-input", "rounded-circle");
+  input.type = "checkbox";
+  input.name = "tasks";
+  input.value = name;
+  input.id = taskId;
+  input.checked = complete;
+  const label = document.createElement("label");
+  label.classList.add("form-check-label", "fw-bold");
+  label.htmlFor = taskId;
+  label.innerText = name;
+  d.appendChild(input);
+  d.appendChild(label);
+
+  const badge = document.createElement("span");
+  badge.classList.add("badge", "d-flex", "align-items-center");
+  badge.innerText = category;
+  switch (category) {
+    case "URGENT":
+      badge.classList.add("text-bg-warning");
+      break;
+    case "NEW":
+      badge.classList.add("text-bg-success");
+      break;
+    default:
+      badge.classList.add("text-bg-secondary");
+  }
+  li.appendChild(d);
+  li.appendChild(badge);
   return li;
 }
